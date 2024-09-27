81d953e9a18bdfde76377491f0bb91757e91da6a
Sheargrub <sheargrub@gmail.com>
2024-09-27 04:55:05 -0500

    Interpreter: refactor to object

--- src/lox_interpreter.rs
+++ src/lox_interpreter.rs
@@ -6,63 +6,79 @@ use lox_expression::Expression::*;
 use lox_node::*;
 use lox_node::Literal::*;
 
-pub fn evaluate(e: Expression) -> Result<Literal, String> {
-    match e {
-        LExp(l) => Ok(l),
-        UExp(u) => evaluate_unary(u),
-        BExp(b) => evaluate_binary(b),
-        Grouping(boxed_exp) => evaluate(*boxed_exp),
-    }
+struct LoxInterpreter {
+
 }
 
-fn evaluate_unary(u: Unary) -> Result<Literal, String> {
-    match u {
-        Unary::Negative(expr) => {
-            match *expr {
-                LExp(l) => Ok(Number(-get_number(l)?)),
-                other => {
-                    let inner = Expression::boxed_literal(evaluate(other)?);
-                    evaluate_unary(Unary::Negative(inner))
-                },
-            }
-        },
-        Unary::Not(expr) => {
-            if let LExp(l) = *expr { Ok(Boolean(is_truthful(l))) }
-            else {
-                let inner = Expression::boxed_literal(evaluate(*expr)?);
-                evaluate_unary(Unary::Not(inner))
-            }
-        },
+impl LoxInterpreter {
+    pub fn new() -> LoxInterpreter {
+        LoxInterpreter{}
     }
-}
 
-fn evaluate_binary(b: Binary) -> Result<Literal, String> {
-    use lox_node::BinaryOp::*;
-    
-    let left = evaluate(*b.left)?;
-    let right = evaluate(*b.right)?;
-
-    match b.operator {
-        Add => {
-            let error_str = format!("Attempted to add mismatched operands {:?} and {:?}.", left, right);
-            match (left, right) {
-                (Number(m), Number(n)) => Ok(Number(m + n)),
-                (StringData(s), StringData(t)) => Ok(StringData(format!("{}{}", s, t))),
-                _ => Err(error_str),
-            }
+    pub fn evaluate_stmt(&self, s: Statement) -> Result<Literal, String> {
+        match s {
+            _ => Err(String::new()) // temp
         }
-        Subtract => Ok(Number(get_number(left)? - get_number(right)?)),
-        Multiply => Ok(Number(get_number(left)? * get_number(right)?)),
-        Divide => Ok(Number(get_number(left)? / get_number(right)?)),
-        Modulo => Ok(Number(get_number(left)? % get_number(right)?)),
+    }
 
-        Less => Ok(Boolean(get_number(left)? < get_number(right)?)),
-        LessEqual => Ok(Boolean(get_number(left)? <= get_number(right)?)),
-        Greater => Ok(Boolean(get_number(left)? > get_number(right)?)),
-        GreaterEqual => Ok(Boolean(get_number(left)? >= get_number(right)?)),
+    pub fn evaluate_expr(&self, e: Expression) -> Result<Literal, String> {
+        match e {
+            LExp(l) => Ok(l),
+            UExp(u) => self.evaluate_expr_unary(u),
+            BExp(b) => self.evaluate_expr_binary(b),
+            Grouping(boxed_exp) => self.evaluate_expr(*boxed_exp),
+        }
+    }
 
-        Equal => Ok(Boolean(left == right)),
-        NotEqual => Ok(Boolean(left != right)),
+    fn evaluate_expr_unary(&self, u: Unary) -> Result<Literal, String> {
+        match u {
+            Unary::Negative(expr) => {
+                match *expr {
+                    LExp(l) => Ok(Number(-get_number(l)?)),
+                    other => {
+                        let inner = Expression::boxed_literal(self.evaluate_expr(other)?);
+                        self.evaluate_expr_unary(Unary::Negative(inner))
+                    },
+                }
+            },
+            Unary::Not(expr) => {
+                if let LExp(l) = *expr { Ok(Boolean(is_truthful(l))) }
+                else {
+                    let inner = Expression::boxed_literal(self.evaluate_expr(*expr)?);
+                    self.evaluate_expr_unary(Unary::Not(inner))
+                }
+            },
+        }
+    }
+
+    fn evaluate_expr_binary(&self, b: Binary) -> Result<Literal, String> {
+        use lox_node::BinaryOp::*;
+        
+        let left = self.evaluate_expr(*b.left)?;
+        let right = self.evaluate_expr(*b.right)?;
+
+        match b.operator {
+            Add => {
+                let error_str = format!("Attempted to add mismatched operands {:?} and {:?}.", left, right);
+                match (left, right) {
+                    (Number(m), Number(n)) => Ok(Number(m + n)),
+                    (StringData(s), StringData(t)) => Ok(StringData(format!("{}{}", s, t))),
+                    _ => Err(error_str),
+                }
+            }
+            Subtract => Ok(Number(get_number(left)? - get_number(right)?)),
+            Multiply => Ok(Number(get_number(left)? * get_number(right)?)),
+            Divide => Ok(Number(get_number(left)? / get_number(right)?)),
+            Modulo => Ok(Number(get_number(left)? % get_number(right)?)),
+
+            Less => Ok(Boolean(get_number(left)? < get_number(right)?)),
+            LessEqual => Ok(Boolean(get_number(left)? <= get_number(right)?)),
+            Greater => Ok(Boolean(get_number(left)? > get_number(right)?)),
+            GreaterEqual => Ok(Boolean(get_number(left)? >= get_number(right)?)),
+
+            Equal => Ok(Boolean(left == right)),
+            NotEqual => Ok(Boolean(left != right)),
+        }
     }
 }
 
@@ -78,8 +94,6 @@ fn get_number(l: Literal) -> Result<f64, String> {
     if let Number(n) = l { Ok(n) }
     else { Err(format!("Attempted to use literal {:?} in place of a Number.", l)) }
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -99,7 +113,8 @@ mod tests {
 
     fn test_expression_generic(s: &str, expected: Literal) {
         let expr = string_to_expr(s);
-        let result = evaluate(expr).expect("Evaluation error");
+        let intp = LoxInterpreter::new();
+        let result = intp.evaluate_expr(expr).expect("Evaluation error");
         assert_eq!(expected, result, "Expected to recieve left side; recieved right.");
     }
 
